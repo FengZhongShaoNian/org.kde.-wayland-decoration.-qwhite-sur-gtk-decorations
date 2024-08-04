ccad76a285bd7f5d24ec72081d30087ef45e2cb2
fengzhongshaonian <2844126834@qq.com>
2024-08-04 12:55:45 +0800

    modify script

--- install.sh
+++ install.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-has_flatpak_builder(){
-    flatpak list --app --columns=application | grep org.flatpak.Builder &> /dev/null || return 1
+has_flatpak_app(){
+    flatpak list --app --columns=application | grep "$1"  &> /dev/null || return 1
 }
 
-if ! has_flatpak_builder; then
-    echo 'Install org.flatpak.Builder with flathub...'
+has_flatpak_runtime(){
+    flatpak list --runtime --columns=application,branch | grep "$1" | grep "$2"  &> /dev/null || return 1
+}
+
+if ! has_flatpak_app org.flatpak.Builder; then
+    echo 'Installing org.flatpak.Builder with flathub...'
     flatpak install -y flathub org.flatpak.Builder
+else
+    echo 'org.flatpak.Builder exists.'
 fi
-mkdir build
+
+if ! has_flatpak_runtime org.kde.Sdk 5.15-23.08; then
+    echo 'Installing org.kde.Sdk/x86_64/5.15-23.08 with flathub...'
+    flatpak install -y flathub org.kde.Sdk/x86_64/5.15-23.08
+else
+    echo 'org.kde.Sdk/x86_64/5.15-23.08 exists.'
+fi
+
+if [ ! -d ./build ];then
+    mkdir build
+fi
+
 flatpak run org.flatpak.Builder --force-clean --install --user ./build ./org.kde.WaylandDecoration.QWhiteSurGtkDecorations.json
+
+echo 'All Finished.'
