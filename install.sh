eb476bc38d6c6fb54fbf47df70b7e6861bca5100
fengzhongshaonian <2844126834@qq.com>
2025-12-07 19:37:16 +0800

    添加打包.flatpak文件的功能

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+has_fuse2(){
+    pacman -Qs fuse2 &> /dev/null
+}
+
 has_flatpak_app(){
     flatpak list --app --columns=application | grep "$1"  &> /dev/null || return 1
 }
 
-has_flatpak_runtime(){
-    flatpak list --runtime --columns=application,branch | grep "$1" | grep "$2"  &> /dev/null || return 1
+has_kde_platform_runtime(){
+    flatpak list --runtime --columns=application,branch | grep "org.kde.Platform" | grep "$1"  &> /dev/null || return 1
+}
+
+has_kde_sdk_runtime(){
+    flatpak list --runtime --columns=application,branch | grep "org.kde.Sdk" | grep -v "org.kde.Sdk.Locale" | grep "$1"  &> /dev/null || return 1
 }
 
 if ! has_flatpak_app org.flatpak.Builder; then
@@ -16,14 +24,14 @@ else
 fi
 
 readonly sdk_version='6.8'
-if ! has_flatpak_runtime org.kde.Platform "$sdk_version"; then
+if ! has_kde_platform_runtime "$sdk_version"; then
     echo "Installing org.kde.Platform/x86_64/$sdk_version with flathub..."
-    sudo flatpak install -y "flathub org.kde.Platform/x86_64/$sdk_version"
+    sudo flatpak install -y flathub "org.kde.Platform/x86_64/$sdk_version"
 else
     echo "org.kde.Platform/x86_64/$sdk_version exists."
 fi
 
-if ! has_flatpak_runtime org.kde.Sdk "$sdk_version"; then
+if ! has_kde_sdk_runtime "$sdk_version"; then
     echo "Installing org.kde.Sdk/x86_64/$sdk_version with flathub..."
     sudo flatpak install -y flathub org.kde.Sdk/x86_64/$sdk_version
 else
@@ -35,5 +43,7 @@ if [ ! -d ./build ];then
 fi
 
 flatpak run org.flatpak.Builder --force-clean --install --user ./build ./org.inqlude.WaylandDecoration.QWhiteSurGtkDecorations.yml
+
+flatpak build-bundle --runtime ~/.local/share/flatpak/repo "QWhiteSurGtkDecorations-$sdk_version.flatpak"  org.inqlude.WaylandDecoration.QWhiteSurGtkDecorations "$sdk_version"
 
 echo 'All Finished.'
